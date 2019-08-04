646a283b19c7fcffa32b42ea6460f8dd1b0ac251
Nursultan <nursultan_imanov@mail.ru>
2019-08-04 21:26:54 +0200

    [BackEnd] remove spring security

---
+++ backend/src/main/java/hello/User.java
@@ -0,0 +1,16 @@
+package hello;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String password;
+
+}
